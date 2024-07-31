dbc7362ec706ca9c159470481dcb14bb007fe21f
mohashafici <mohashafici44@gmail.com>
2024-07-31 10:47:06 +0300

    add effect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import profileImage from '../screenshots/ProfileImage.jpeg';
 
 const Home = () => {
+  const [titleIndex, setTitleIndex] = useState(0);
+  const titles = ["Full Stack Developer", "Web Developer", "Project Manager"];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTitleIndex((prevIndex) => (prevIndex + 1) % titles.length);
+    }, 2000); // Change every 2 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
       <div className="text-center">
@@ -13,9 +24,9 @@ const Home = () => {
         />
         <h1 className="text-5xl font-bold">Hi, I'm Mohamed Shafici</h1>
         <p className="text-xl mt-4">
-          A  <span className="text-teal-400">Full Stack Developer</span> with 10 years of commercial experience creating successful websites.
+          A <span className="text-teal-400">{titles[titleIndex]}</span> with 1 years of commercial experience creating successful websites.
         </p>
-        <Link to="/projects"> {/* Use Link to navigate to the Projects page */}
+        <Link to="/projects">
           <button className="mt-6 px-6 py-2 bg-teal-400 text-gray-900 rounded hover:bg-teal-500">
             See my projects I built so far!
           </button>
